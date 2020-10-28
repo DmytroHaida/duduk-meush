517124141c6ed2e2bfab82d8c49f921792795421
DmytroHaida <dmytro.haida@gmail.com>
2020-10-28 20:14:24 +0200

    Updates

---
+++ precache-manifest.0f2587d5559ccfaa584fe724d40c478e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5214a60145bd72c70e8fb4e021856d01",
+    "url": "/duduk-meush/index.html"
+  },
+  {
+    "revision": "79a951b2cf9c9802b8b7",
+    "url": "/duduk-meush/static/css/main.4b7655ff.chunk.css"
+  },
+  {
+    "revision": "60262506ed0aa5561d36",
+    "url": "/duduk-meush/static/js/2.46c53b8c.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/duduk-meush/static/js/2.46c53b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a951b2cf9c9802b8b7",
+    "url": "/duduk-meush/static/js/main.66ff441e.chunk.js"
+  },
+  {
+    "revision": "22b748d11fee57890e13",
+    "url": "/duduk-meush/static/js/runtime-main.2eec6725.js"
+  },
+  {
+    "revision": "0b01dcf19d1de60b12abe1604f32aade",
+    "url": "/duduk-meush/static/media/Logo.0b01dcf1.svg"
+  },
+  {
+    "revision": "01e535a9c2fb17b5fadf841e17f7ba45",
+    "url": "/duduk-meush/static/media/facebook-messenger-white.01e535a9.svg"
+  },
+  {
+    "revision": "4b41c85111a23b462d2e207dfb8bd9ff",
+    "url": "/duduk-meush/static/media/placeImage.4b41c851.jpg"
+  }
+]);
